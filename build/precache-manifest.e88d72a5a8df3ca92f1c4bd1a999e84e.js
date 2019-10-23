bb0e2b7b707201bd52fa28351d0f8b40d4595f34
justwiesek <jwiesek@gmail.com>
2019-10-23 12:24:56 +0200

    app update

---
+++ build/precache-manifest.e88d72a5a8df3ca92f1c4bd1a999e84e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fe8b46672f98d0450d2c27e0a07f23",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f3d11dcf69779abf1a81",
+    "url": "/react-app/static/css/main.89a5cfba.chunk.css"
+  },
+  {
+    "revision": "5fd6ee416da453eb96c3",
+    "url": "/react-app/static/js/2.7908a500.chunk.js"
+  },
+  {
+    "revision": "f3d11dcf69779abf1a81",
+    "url": "/react-app/static/js/main.51024978.chunk.js"
+  },
+  {
+    "revision": "1679d5f8ecc34b13c7cc",
+    "url": "/react-app/static/js/runtime-main.464671de.js"
+  },
+  {
+    "revision": "ff81fd78e7dc55cb65fd97859de03ada",
+    "url": "/react-app/static/media/icon-home.ff81fd78.jpg"
+  },
+  {
+    "revision": "28f560e3b813004b37b416f8502b6cec",
+    "url": "/react-app/static/media/info-img.28f560e3.jpg"
+  },
+  {
+    "revision": "d862677e17870661bc8c87275dc9f918",
+    "url": "/react-app/static/media/logo.d862677e.png"
+  },
+  {
+    "revision": "c5565f03c7a004a73edb077e45087cc8",
+    "url": "/react-app/static/media/railay-beach.c5565f03.jpg"
+  },
+  {
+    "revision": "03c599d296ea37d74f671d2a8254dfa8",
+    "url": "/react-app/static/media/tijuana.03c599d2.jpg"
+  },
+  {
+    "revision": "cc78a0666d5f7c8c70e00987c73e15f4",
+    "url": "/react-app/static/media/waikiki.cc78a066.jpeg"
+  }
+]);
